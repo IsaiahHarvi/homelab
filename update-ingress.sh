78b577e1a0a838de5afd07ce5ed7f86116547597
isaiahharvi <harvillersaiah@gmail.com>
2025-05-04 19:48:07 -0500

    cleanup

--- update-ingress.sh
+++ update-ingress.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# get your current external IP (use whatever service you like)
 IP=$(curl -fsS https://ifconfig.me)
 
 # annotate the ingress in the default namespace
--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 # todo 
+
 microk8s helm3 upgrade --install orion services/charts/orion \
   --set ingress.enabled=true
 
